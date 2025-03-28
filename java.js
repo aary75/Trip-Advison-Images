17a197a212d0c08f11dc9b8a6a125178bd8f0ab4
aary75 <101628957+aary75@users.noreply.github.com>
2025-03-28 10:52:24 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+var hello = document.getElementById('search');
+hello.addEventListener('keydown',function() {console.log('Keyboard Pressed');
+});
+
+// function satAaryan(){
+//     alert("Hiii");
+// }
+
+// hello.addEventListener('click',Aaryan)
